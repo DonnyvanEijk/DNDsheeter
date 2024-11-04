b2f7a313049fd61d161faf1d4dcc289850889949
DonnyvanEijk <donnyvaneijk873@gmail.com>
2024-11-04 21:32:04 +0100

    i ain't doing allat

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/DNDsheeter" : "",
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  }
-  /* config options here */
+
 };
 
 export default nextConfig;
