f1b3e406100641942edfba8486ed5c3ed8988cfa
DonnyvanEijk <donnyvaneijk873@gmail.com>
2024-11-04 21:08:28 +0100

    Nav system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,27 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-     Hiiii marc
-      <div>
-        <ThemeSwapper/>
-      </div>
+       <div className="flex flex-row">
+      {/* Navdiv */}
+          <div className="p-5">
+                 {/* Theme swapping linksbovenin */}
+             <ThemeSwapper/>
+          </div>
+               {/* De rest er naast!!! */}
+          <div className="flex justify-between flex-row items-center w-full mx-5">
+              <div>
+                {/* Navitem */}
+                yes
+              </div>
+              <div>
+                Ja
+              </div>
+              <div>
+                Nee
+              </div>
+          </div>
     </div>
+    </div>
+   
   );
 }
