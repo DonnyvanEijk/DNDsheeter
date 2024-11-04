317578a826c27d9ed3075ad654e8e377ff4ef6dd
DonnyvanEijk <donnyvaneijk873@gmail.com>
2024-11-04 21:02:45 +0100

    Theme swapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { ThemeSwapper } from "@/components/ui/theme-swapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
      Hiiii marc
+      <div>
+        <ThemeSwapper/>
+      </div>
     </div>
   );
 }
