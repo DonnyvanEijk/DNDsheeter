3a8beaac65c50f15017c575df361ed0969dd06d7
DonnyvanEijk <donnyvaneijk873@gmail.com>
2024-11-04 21:29:05 +0100

    image fix ig?

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/DNDsheeter" : "",
   output: 'export',
   distDir: 'dist',
   images: {
